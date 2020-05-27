8ece065fd9c8dae8c53317e0bd82da735fb87c99
Verja <vverja@gmail.com>
2020-05-27 18:00:23 +0300

    Работаем с админкой и постами

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Post (models.Model):
     STATUS_CHOICES = (('draft','Draft'),
@@ -15,7 +16,7 @@ class Post (models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
-   #Metadata
+    # Metadata
     class Meta:
         ordering = ['-publish']
 
