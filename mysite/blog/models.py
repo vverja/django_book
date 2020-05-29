49f955d2772a6b6b0e5edafaf7b8d4dd8e4d6b04
dev <vverja@gmail.com>
2020-05-29 12:02:00 +0300

    Делаем вьюхи

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 
 
 # Create your models here.
-class Post (models.Model):
+class Post(models.Model):
     STATUS_CHOICES = (('draft','Draft'),
                       ('published','Published'))
     title = models.CharField(max_length=250)
@@ -15,6 +21,13 @@ class Post (models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month,
+                                                 self.publish.day, self.slug])
 
     # Metadata
     class Meta:
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 
+app_name = 'blog'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog'))
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
